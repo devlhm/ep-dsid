f389227f8699d570f100751e25ce77eb6f17b1ac
PdrFernandes <pedro.eu.of@gmail.com>
2025-03-21 14:04:28 -0300

    updae peer listing functionality

--- src/main/java/eachare/commands/ListPeers.java
+++ src/main/java/eachare/commands/ListPeers.java
@@ -1,7 +1,9 @@
 package eachare.commands;
 
+import eachare.CommandHandler;
 import eachare.NeighborList;
 import eachare.Peer;
+import java.util.Scanner;
 
 public class ListPeers implements Command {
 
@@ -20,5 +22,29 @@ public class ListPeers implements Command {
             System.out.println("[" + index + "] " + peer.getIpAddress() + ":" + peer.getPort() + " " + peer.getStatus());
             index++;
         }
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int commandIdx;
+            if (sc.hasNextInt()) {
+                commandIdx = sc.nextInt();
+
+                if (commandIdx == 0) break;
+
+                if (commandIdx > 0 && commandIdx <= neighbors.getAll().size()) {
+                    Peer peer = neighbors.get(commandIdx - 1);
+                    System.out.println("encaminhando mensagem " + peer.getIpAddress() + ":" + peer.getPort());
+
+                    //TODO: alterar para o envio de mensagem pelo socket
+                } else {
+                    System.out.println("Indice inválido! Escolha um número entre 1 e " + neighbors.getAll().size());
+                }
+            } else {
+                System.out.println("Entrada inválida! Digite um número.");
+                sc.next();
+            }
+        }
+
+        CommandHandler.showMenu();
     }
 }
